82904f6e83a9e5b6566dfc7f8eee214c86fca5cf
a95thurow <a95thurow@ufl.edu>
2015-02-23 01:08:38 -0500

    Signin/Signup fixes, more to come

--- public/modules/calendar/controllers/main.js
+++ public/modules/calendar/controllers/main.js
@@ -7,6 +7,23 @@
  * # MainCtrl
  * Controller of the angularBootstrapCalendarApp
  */
+
+angular.module('mwl.calender').controller('HeaderController', ['$scope', 'Authentication', 'Menus',
+  function($scope, Authentication, Menus) {
+    $scope.authentication = Authentication;
+    $scope.isCollapsed = false;
+    $scope.menu = Menus.getMenu('topbar');
+
+    $scope.toggleCollapsibleMenu = function() {
+      $scope.isCollapsed = !$scope.isCollapsed;
+    };
+
+    // Collapsing the menu after navigation
+    $scope.$on('$stateChangeSuccess', function() {
+      $scope.isCollapsed = false;
+    });
+  }
+]);
 angular.module('mwl.calendar')
   .controller('MainCtrl', function ($scope, $modal, moment) {
 
