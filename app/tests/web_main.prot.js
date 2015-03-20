4bf15f98b052808268dc49d65c025663bcf769af
amstewart <alex.stewart@ufl.edu>
2015-03-20 04:22:34 -0400

    Did something... I don't remember.

--- app/tests/web_main.prot.js
+++ app/tests/web_main.prot.js
@@ -1,22 +1,24 @@
-﻿'use strict';
+'use strict';
 
-//var assert = require('assert');
+var assert = require('assert');
 
 var f_homebutton = function () {
 	$('.navbar-brand').click();
-}
+};
 
 var f_adminbutton = function () {
-	var buttons = elment(by.class('ui-route')).all(by.tagName('a'));
+	var buttons = element(by.css('.ng-scope')).all(by.linkText('Admin Page'));
 	buttons.get(0).click();
-}
+};
 
 describe ('Protractor front-end, web tests:', function() {
 
 	describe ('Basic funcitonality:', function() {
 		it ('Server connection', function() {
 			browser.get('http://localhost:3000/');
-			expect(browser.getTitle()).toEqual('COE Event Tracker');
+			//expect(browser.getTitle()).toEqual('COE Event Tracker');
+			assert.equal(browser.getTitle(), 'COE Event Tracker');
+			//assert.equal(webdriver.getTitle(), 'COE Event Tracker');
 		});
 	});
 
@@ -33,7 +35,7 @@ describe ('Protractor front-end, web tests:', function() {
 		it ('Admin Page', function() {
 			f_homebutton();
 			f_adminbutton();
-		})
+		});
 	});
 });
 
