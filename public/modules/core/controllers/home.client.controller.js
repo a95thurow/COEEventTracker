ccf3dc26974a9469f1e3a1fc1109f4e4760d5e7b
a95thurow <a95thurow@ufl.edu>
2015-03-23 00:56:55 -0400

    Events have details, edit page not functional yet

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,17 +5,9 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
-    	$scope.posts = [
-    	{title: 'Event one!', Details: 'These are awesome details'},
-    	{title: 'Event two!', Details: 'Awesome Details 2'}
-    	];
-    	$scope.addevent = function(){
-    		  posts.create({
-    				title: $scope.title,
-    				Details: $scope.Details,
- 			  });
-    		$scope.title = '';
-    		$scope.Details = '';
-    	}
+        $scope.find = function() {
+            $scope.events = Events.query();
+        };
+
 	}
 ]);
