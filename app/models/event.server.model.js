4f4f0f0ae2efcafaa93b67aac3834f9605268c0e
a95thurow <a95thurow@ufl.edu>
2015-03-22 20:10:27 -0400

    Setting up Event module, need login

---
+++ app/models/event.server.model.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Event Schema
+ */
+var EventSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		trim: true
+	}
+});
+
+mongoose.model('Event', EventSchema);
