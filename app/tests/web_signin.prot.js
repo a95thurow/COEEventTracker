462d4e0e0c984d2174fced94aaed3a9163c7de6e
kevinqmcdonald <kevinqmcdonald@gmail.com>
2015-03-23 02:09:53 -0400

    Added another test

--- app/tests/web_signin.prot.js
+++ app/tests/web_signin.prot.js
@@ -1,16 +1,43 @@
 'use strict';
 
 describe ('Protractor sign in tests', function() {
+	var enter = browser.actions().sendKeys(protractor.Key.ENTER);
+	var username = element(by.id('username'));
+	var password = element(by.id('password'));
+	var error = element(by.binding('error'));
+	var signinURL;
+
 	beforeEach(function() {
 		browser.get('http://localhost:3000/#!/signin');
 	});
 
-	it('should successfully sign in test user', function() {
+	it('should not redirect upon incorrect login AND should display error message', function() {
+		username.clear();
+		password.clear();
+
+		username.sendKeys('test');
+		password.sendKeys('test');
+
+		signinURL = browser.getCurrentUrl();
+
+		enter.perform();
+
+		expect(browser.getCurrentUrl()).toEqual(signinURL);
+		expect(error.getText()).toEqual('Unknown user or invalid password');
+	});
+
+	it('should successfully sign in test user and redirect to home page', function() {
+
+		username.clear();
+		password.clear();
 		
-		element(by.name('username')).sendKeys('jimjohnson');
+		username.sendKeys('jimjohnson');
+		password.sendKeys('jimjohnson');
 
-		element(by.name('password')).sendKeys('jimjohnson');
+		signinURL = browser.getCurrentUrl();
 
+		enter.perform();
 
+		expect(browser.getCurrentUrl()).not.toEqual(signinURL);
 	});
 });
