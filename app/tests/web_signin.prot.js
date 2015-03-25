dc04af63f7a080bd13ba650be3970e0f670f7f8a
amstewart <alex.stewart@ufl.edu>
2015-03-24 23:23:47 -0400

    Working on additional sign in tests

--- app/tests/web_signin.prot.js
+++ app/tests/web_signin.prot.js
@@ -1,14 +1,29 @@
 'use strict';
 
-describe ('Protractor sign in tests', function() {
+describe ('Administration Sign In:', function() {
 	var enter = browser.actions().sendKeys(protractor.Key.ENTER);
 	var username = element(by.id('username'));
 	var password = element(by.id('password'));
 	var error = element(by.binding('error'));
-	var signinURL;
+	var signinURL;// = browser.getCurrentUrl();
 
 	beforeEach(function() {
 		browser.get('http://localhost:3000/#!/signin');
+	});
+
+	describe('No Credentials:', function() {
+		username.clear();
+		password.clear();
+
+		enter.perform();
+
+		it ('Page does not redirect.', function() {
+			expect(browser.getCurrentUrl()).toEqual(signinURL);
+		});
+
+		it ('Displays no credentials error.', function() {
+			expect(error.getText()).toEqual('Missing credentials');
+		});
 	});
 
 	it('should not redirect upon incorrect login AND should display error message', function() {
