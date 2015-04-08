a87c15ff306721e02b767e11da63a1715f34c029
amstewart <alex.stewart@ufl.edu>
2015-04-08 17:52:59 -0400

    Renamed anonymous navigation test and updated protractor config to reflec the rename.

--- protractor_conf.js
+++ protractor_conf.js
@@ -75,7 +75,7 @@ exports.config = {
   // Spec patterns are relative to the location of this config.
   specs: [
 	//'app/tests/web_event_create.prot.js',
-	'app/tests/web_nav.prot.js',
+	'app/tests/web_nav_anon.prot.js',
 	//'app/tests/page_home.prot.js',
 	//'app/tests/web_signin.prot.js'
   ],
